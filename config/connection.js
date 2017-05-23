d54d03fe8727bf1fccc5df6664e6baba8a6de8b8
Cherish Khang <cherishkhang@gmail.com>
2017-05-22 20:51:21 -0400

    Updates

--- config/connection.js
+++
@@ -1,7 +0,0 @@
-exports.connection = {
-	database: 'qlokjgdi6fu91pld',
-	host: 'mwgmw3rs78pvwk4e.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-	username: 'wll4lp2o4xjuyokl',
-	password: 'ladcc498t91fysyb',
-	port: 3306
-}
